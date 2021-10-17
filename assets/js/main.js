c86fd9a5514fe9eedca1b7170ba4258c0eef302d
hasan.mehedi <hasan.mehedi@bjitgroup.com>
2021-10-18 03:44:46 +0600

    navbar done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,42 @@
-/*=============== SHOW MENU ===============*/
+/*==================== SHOW MENU ====================*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== HOME SWIPER ===============*/
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
 
 /*=============== NEW SWIPER ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
